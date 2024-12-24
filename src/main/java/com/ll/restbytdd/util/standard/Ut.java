47563607ed7812101af682bac1e6231537277148
Metronon <bumpercar07@gmail.com>
2024-12-24 14:38:40 +0900

    join g

--- src/main/java/com/ll/restbytdd/util/standard/Ut.java
+++
@@ -1,9 +0,0 @@
-package com.ll.restbytdd.util.standard;
-
-public class Ut {
-    public static class str {
-        public static boolean isBlank(String str) {
-            return str == null || str.trim().isEmpty();
-        }
-    }
-}
