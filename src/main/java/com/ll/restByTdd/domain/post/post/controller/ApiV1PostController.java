97c0519d0d0bf784fdbd6e8864bc873934aa80af
Metronon <bumpercar07@gmail.com>
2025-01-02 15:44:30 +0900

    post modify test

--- src/main/java/com/ll/restByTdd/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/restByTdd/domain/post/post/controller/ApiV1PostController.java
@@ -7,9 +7,10 @@ import com.ll.restByTdd.domain.post.post.service.PostService;
 import com.ll.restByTdd.global.rq.Rq;
 import com.ll.restByTdd.global.rsData.RsData;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -46,6 +47,40 @@ public class ApiV1PostController {
         return new RsData<>(
                 "201-1",
                 "%d번 글이 작성되었습니다.".formatted(post.getId()),
+                new PostDto(post)
+        );
+    }
+
+
+    record PostModifyReqBody(
+            @NotBlank
+            @Length(min = 2, max = 100)
+            String title,
+            @NotBlank
+            @Length(min = 2, max = 10000000)
+            String content
+    ) {
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public RsData<PostDto> modify(
+            @PathVariable long id,
+            @RequestBody @Valid PostModifyReqBody reqBody
+    ) {
+        Member actor = rq.checkAuthentication();
+
+        Post post = postService.findById(id).get();
+
+        post.checkActorCanModify(actor);
+
+        postService.modify(post, reqBody.title, reqBody.content);
+
+        postService.flush();
+
+        return new RsData<>(
+                "200-1",
+                "%d번 글이 수정되었습니다.".formatted(id),
                 new PostDto(post)
         );
     }
